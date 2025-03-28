f88a36bf822a3cf9e3f16cf95d661867327864bc
Guilherme Claudino <guilhermeclaudino1997@gmail.com>
2025-03-27 22:20:45 -0300

    Correcting camera rotation and zoom, initial creation of enemy spawner

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,31 +1,54 @@
 using System;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Vector3 = UnityEngine.Vector3;
 
 public class CameraManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private Camera cm;
     [FormerlySerializedAs("sensitivity")] [SerializeField,Range(1f,10f)] private float editorSensitivity;
+    [SerializeField,Range(1f,10f)] private float zoomSensitivity;
     private float sensitivityCode;
+    private float zoomCode;
+    private float cameraCode;
+    private Vector3 velocity;
+    [SerializeField] private Player player;
+    [SerializeField,Range(1f,10f)] private float cameraSpeed;
+    [SerializeField] private float minDistance;
+    [SerializeField] private float maxDistance;
     private void Start()
     {
-        if (cm == null)
-        {
-            cm = Camera.main;
-        }
-        sensitivityCode = editorSensitivity / 10f;
+        cm ??= Camera.main;
+        ApplyScale();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float angle = Input.mousePositionDelta.x*sensitivityCode;
-        transform.Rotate(Vector3.up, angle);
+        Vector3 cameraPosition = cm.transform.position;
+        Vector3 playerPosition = player.transform.position;
+        float mouseWheelY = Input.mouseScrollDelta.y; 
+        float angle = -Input.mousePositionDelta.x * sensitivityCode;
+        player.transform.Rotate(Vector3.up, -angle, Space.Self);
+        transform.Rotate(Vector3.forward, angle, Space.Self);
+        float distance = Mathf.Clamp(cameraPosition.y+mouseWheelY * zoomCode,minDistance,maxDistance);
+        Vector3 targetPosition = new Vector3(playerPosition.x, distance , playerPosition.z);
+        //Vector3.SmoothDamp(cameraPosition, targetPosition, ref velocity, 0.1f);
+        //transform.position = velocity*cameraSpeed+cameraPosition;
+        transform.position = targetPosition;
     }
 
     private void OnValidate()
     {
+        ApplyScale();
+    }
+
+    private void ApplyScale()
+    {
         sensitivityCode = editorSensitivity / 10f;
+        zoomCode = zoomSensitivity / 10f;
+        cameraCode = cameraSpeed / 100f;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine.Serialization;
 public class Player : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
-    [FormerlySerializedAs("acceleration")] [SerializeField] private float velocity;
+    [FormerlySerializedAs("velocity")] [FormerlySerializedAs("acceleration")] [SerializeField] private float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,19 +16,19 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(gameObject.transform.forward * velocity, ForceMode.VelocityChange);
+            rb.AddForce(gameObject.transform.forward * speed, ForceMode.VelocityChange);
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            rb.AddForce(gameObject.transform.forward * -velocity, ForceMode.VelocityChange);
+            rb.AddForce(gameObject.transform.forward * -speed, ForceMode.VelocityChange);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(gameObject.transform.right * -velocity, ForceMode.VelocityChange);
+            rb.AddForce(gameObject.transform.right * -speed, ForceMode.VelocityChange);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            rb.AddForce(gameObject.transform.right * velocity, ForceMode.VelocityChange);
+            rb.AddForce(gameObject.transform.right * speed, ForceMode.VelocityChange);
         }
     }
 }
