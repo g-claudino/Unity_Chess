78e6b229f3b1a7c24a987a36f7a3504db7350020
Guilherme Claudino <guilhermeclaudino1997@gmail.com>
2025-04-05 18:57:17 -0300

    Adding enemy movement logics and correcting enemy spawning

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,17 @@
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Enemy : MonoBehaviour
 {
-    float currTime = 0f;
+    float currTime = 0f;    
+    private Player player = null;
+    private Vector3 playerPosition;
+    private Vector3 enemyPosition;
+    private Vector3 deltaPosition;
+    private bool activeEnemy = false;
+    [SerializeField] private Rigidbody rb;
+    [FormerlySerializedAs("velocity")] [SerializeField, Range(0f, 2f)] private float speed;
     [SerializeField,Range(10f,100f)] private float LifeSpan;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,16 +22,37 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!activeEnemy) return;
+        
         currTime += Time.deltaTime;
-        if (currTime >= LifeSpan)
+        TrackPlayerPosition();
+        RemoveFromPlay(currTime);
+    }
+
+    void TrackPlayerPosition()
+    {
+        if (player == null)
         {
-            RemoveFromPlay();
+            player = FindObjectsByType<Player>(FindObjectsInactive.Exclude,FindObjectsSortMode.None)[0];
+        }
+        playerPosition = player.transform.position;
+        enemyPosition = transform.position;
+        deltaPosition = playerPosition - enemyPosition;
+        deltaPosition.Normalize();
+        rb.AddForce(deltaPosition*speed, ForceMode.VelocityChange);
+    }
+    
+    void RemoveFromPlay(float passedTime)
+    {
+        if (passedTime >= LifeSpan)
+        {
+            activeEnemy = false;
+            Destroy(gameObject);
         }
     }
 
-    void RemoveFromPlay()
+    public void ActivateEnemy()
     {
-        Destroy(transform.gameObject);
+        activeEnemy = true;
     }
-    
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,14 +4,18 @@ public class Spawner : MonoBehaviour
 {
     float currTime = 0f;
     [SerializeField,Range(1f,10f)] private float SpawnTime;
-    [SerializeField] private Vector2 MaximumPositions;
+    [SerializeField,Range(1f,10f)] private float wallMargin;
+    [SerializeField] private Collider arenaFloor;
+    private Collider enemyCollider;
+    private Vector3 MaximumPositions;
     private float rX = 0;
+    private float rY = 0;
     private float rZ = 0;
     [SerializeField] private Enemy SpawnEnemy;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        enemyCollider  = SpawnEnemy.GetComponent<Collider>();
     }
 
     // Update is called once per frame
@@ -20,10 +24,14 @@ public class Spawner : MonoBehaviour
         currTime += Time.deltaTime;
         if (currTime >= SpawnTime)
         {
+            MaximumPositions = arenaFloor.bounds.center+arenaFloor.bounds.extents;
             currTime = 0f;
-            rX = Random.Range(-MaximumPositions.x, MaximumPositions.x);
-            rZ = Random.Range(-MaximumPositions.y, MaximumPositions.y);
-            Enemy clone = Instantiate(SpawnEnemy, new Vector3(rX, transform.position.y, rZ), Quaternion.identity);
+            rX = Random.Range(-MaximumPositions.x+wallMargin, MaximumPositions.x-wallMargin);
+            rY = MaximumPositions.y+enemyCollider.bounds.extents.y;
+            rZ = Random.Range(-MaximumPositions.z + wallMargin, MaximumPositions.z - wallMargin);
+            Enemy clone = Instantiate(SpawnEnemy, new Vector3(rX, rY, rZ),
+                Quaternion.identity);
+            clone.ActivateEnemy();
         }
     }
 }
